43c709a182ac8dae9e4d7975ae7594ed873b48d4
aniol.moya <aniol.moya@auleseebe.upc.edu>
2025-02-13 13:42:36 +0100

    p

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,6 @@
 #Codigo para problema 3
+x <- c(8,6,0,1,7,5,8,1,3,4)#definir vector
+y <- x[-c(3,7,8)]#eliminar posicions del vector x
+y
+sum(exp(x))-sum(exp(y))
+y[2]#mirar valor en la posicio 2 del vector y
