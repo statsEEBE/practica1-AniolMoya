4be06d66e7fea6b8193c8e072a84f6c02cda71f1
aniol.moya <aniol.moya@auleseebe.upc.edu>
2025-02-13 13:02:09 +0100

    p

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,11 @@
 #Codigo para problema 1
-
-plot(1:5)
+mis_dades <- mtcars
+mean(mis_dades$qsec)#promedio
+x <- mis_dades$cyl
+sort(x)#ordena de petit a gran
+median(x)#mediana
+quantile(x, 0.25)#quartile (dades, percentatge)
+IQR(x)#valor interquartilico(entre q0.25 i q0.75)
+boxplot(x)#plot de les dades
+sd(x)#desviacion tipica(dispersion a la mediana)=(sqrt(var(x))
+var(x)#Variancia muestral
